4cee9d667b9895a9671c513e36c88e4762d65ad1
victoria <veny78@ymail.com>
2023-04-20 18:27:15 -0700

    Run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int t;
 long int n;
 long long int p;
 float g;
-printf("Size of a char:%lu byte(s)\n", sizeof(m));
-printf("Size of an int:%lu byte(s)\n", sizeof(t));
-printf("Size of a long int:%lu byte(s)\n", sizeof(n));
-printf("Size of a long long int:%lu byte(s)\n", sizeof(p));
-printf("Size of a float:%lu byte(s)\n", sizeof(g));
+printf("Size of a char: %lu byte(s)\n", sizeof(m));
+printf("Size of an int: %lu byte(s)\n", sizeof(t));
+printf("Size of a long int: %lu byte(s)\n", sizeof(n));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(p));
+printf("Size of a float: %lu byte(s)\n", sizeof(g));
 return (0);
 }
