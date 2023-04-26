30325a7889866789bca3f8884638f20b4a2c2ad4
victoria <veny78@ymail.com>
2023-04-25 22:11:04 -0700

    multiplication table

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* add - adds two nummbers
+* @x: first int to be added
+* @y: second int to be added
+* Return: sum of x and y
+*/
+int add(int x, int y)
+{
+	return (x + y);
+}
