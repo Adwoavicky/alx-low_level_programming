1279a1620d68cbece28050ade52e25386c30f59a
victoria <veny78@ymail.com>
2023-04-25 22:58:50 -0700

    Battery

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @a: start printing from this number
+ */
+
+void print_to_98(int a)
+{
+	if (a <= 98)
+	{
+	for (a = 0; a <= 98; a++)
+	{
+	if (a == 98)
+	{
+	printf("%d", a);
+	printf("\n");
+	break;
+	}
+	else
+	{
+	printf("%d, ", a);
+	}
+	}
+	}
+	else
+	{
+	for (a = 0; a >= 98; a--)
+	{
+	if (a == 98)
+	{
+	printf("%d, ", a);
+	printf("\n");
+	break;
+	}
+	else
+	{
+	printf("%d,", a);
+	}
+	}
+	}
+}
