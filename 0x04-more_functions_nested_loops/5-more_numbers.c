ce5aefe0b4201737b3e8d3c00b0b15f87166594d
victoria <veny78@ymail.com>
2023-04-27 09:05:15 -0700

    Universal language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* more_numbers - prints numbers from 0 to 14, 10 times
+* Return: always sucess
+*/
+
+void more_numbers(void)
+{
+	int m, n;
+
+	for (m = 0; m <= 10; m++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n >= 10)
+			_putchar('l');
+			_putchar(n % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
