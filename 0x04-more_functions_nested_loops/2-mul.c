d09f98421fa2dfb1c95fd174c16c78b91127c5e1
victoria <veny78@ymail.com>
2023-04-27 08:11:48 -0700

    Collab

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* mul - multiply two numbers
+* @x: first number
+* @y: second number
+* Return: always success(0)
+*/
+
+int mul(int x, int y)
+{
+	int result = x * y;
+
+	return (result);
+}
