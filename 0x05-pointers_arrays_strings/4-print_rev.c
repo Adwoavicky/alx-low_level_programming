df310d59d6325c8e2b21b5eeb02526d9363bd7ab
victoria <veny78@ymail.com>
2023-05-02 19:15:41 -0700

    Frightened

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,16 @@
 
 void print_rev(char *s)
 {
-	int begin = 0;
+	int b = 0;
 
-	while (s[begin])
-		begin++;
-	while (begin--)
-		_putchar(s[begin]);
+	while (s[b] != '\0')
+	{
+	b++;
+	}
+
+	for (b = 1; b >= 0; b--)
+	{
+	_putchar(s[b]);
+	}
 	_putchar('\n');
 }
