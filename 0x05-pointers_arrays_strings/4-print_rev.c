4cfc7c5a5af46e7c89d1e32ed9d87135301d2a8e
victoria <veny78@ymail.com>
2023-05-02 19:21:12 -0700

    Reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,16 +8,11 @@
 
 void print_rev(char *s)
 {
-	int b = 0;
+	int begin = 0;
 
-	while (s[b] != '\0')
-	{
-	b++;
-	}
-
-	for (b = 1; b >= 0; b--)
-	{
-	_putchar(s[b]);
-	}
+	while (s[begin])
+		begin++;
+	while (begin--)
+		_putchar(s[begin]);
 	_putchar('\n');
 }
