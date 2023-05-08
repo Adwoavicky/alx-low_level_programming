8671f64c620ac4f09e87bde6fa325166d78349ed
victoria <veny78@ymail.com>
2023-05-08 14:51:25 -0700

    Locate

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* _strstr - function that identifies a substring
+* @haystack: the subsequent string
+* @needle: the initial string
+* Return: a pointer to the beginning substring
+* NULL if such string is not found
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int h;
+
+	if (*needle == 0)
+	{
+		return (*haystack);
+	}
+
+	while (*haystack)
+	{
+		h = 0;
+
+		if (haystack[h] == needle[h])
+		{
+			do {
+				if (needle[h + 1] == '\0')
+					return (haystack);
+				h++;
+
+			} while (haystack[h] == neddle[h]);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
