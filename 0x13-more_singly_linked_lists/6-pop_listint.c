32a4cd76f7fd705df1982dddd0c40f667f2d8ad5
victoria <veny78@ymail.com>
2023-06-06 02:54:31 -0700

    Freed lists

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* pop_listint - a function tha deletes the head node of listint_t
+* @head: the double pointer to be deleted
+* Return: the head node's data, else 0 if otherwise
+*/
+
+int pop_listint(listint_t **head)
+{
+	listint_t k;
+	int m;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+
+	m = (*head)->n;
+	k = (*head)->next;
+	free(*head);
+	*head = k;
+
+	return (m);
+}
