82fad996d396953d78745e09f312276d2a626d9a
victoria <veny78@ymail.com>
2023-06-12 16:32:33 -0700

    Speak gently

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,9 +12,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	int o, w, h = 0;
 
 	if (filename == NULL)
-	
+
 		return (-1);
-	
 
 	if (text_content != NULL)
 	{
@@ -26,7 +25,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	w = write(o, text_content, h);
 
 	if (o == -1 || w == -1)
-	
 		return (-1);
 
 	close(o);
